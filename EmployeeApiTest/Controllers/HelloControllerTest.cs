bc239d18ed7617c60779f51c9be60922d86c38a9
linwenjun <linwenjun@163.com>
2020-11-08 13:22:37 +0000

    first commit

---
+++ EmployeeApiTest/Controllers/HelloControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using EmployeeApi;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+
+namespace EmployeeApiTest.Controllers
+{
+    public class HelloControllerTest
+    {
+        [Fact]
+        public async Task Should_return_hello_world_with_default_request()
+        {
+            // given
+            TestServer server = new TestServer(new WebHostBuilder()
+               .UseStartup<Startup>());
+            HttpClient client = server.CreateClient();
+
+            // when
+            var response = await client.GetAsync("/hello");
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            // then
+            Assert.Equal("Hello World", responseString);
+        }
+    }
+}
---
+++ EmployeeApiTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace EmployeeApiTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
